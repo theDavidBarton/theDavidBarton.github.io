7d85f891cf904a7da496a62bc51359305bf20319
David Barton <barton.david.sv@gmail.com>
2020-01-05 11:32:15 +0100

    reduce load on api during development

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 async function getQuote() {
   try {
     let obj
-    const response = await fetch('https://twin-peaks-api.herokuapp.com/api/quotes/recommend')
-    await response.json().then(function(data) {
-      obj = data[0]
-    })
+    let response
+    // reduce load on twin peaks api during development
+    if (window.location.href.match(/https:\/\/thedavidbarton.github.io\//gi)) {
+      response = await fetch('https://twin-peaks-api.herokuapp.com/api/quotes/recommend')
+      await response.json().then(function(data) {
+        obj = data[0]
+      })
+    } else {
+      response =
+        '[{"quoteTextOnly":"[DEV MODE] The owls are not what they seem.","persons":["The Giant","Person Name"]}]'
+      obj = JSON.parse(response)[0]
+    }
+
     const quoteTextOnly = await obj.quoteTextOnly
     const persons = await obj.persons.map(el => el)
     document.querySelector('#twinpeaks-quote > p').textContent = quoteTextOnly
