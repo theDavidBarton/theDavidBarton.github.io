d533ad460898a97a2d0ef7150c001abd54db1b95
David Barton <barton.david.sv@gmail.com>
2020-01-06 21:36:16 +0100

    looks nicer

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ async function getQuote() {
     // reduce load on twin peaks api during development
     if (window.location.href.match(/https:\/\/thedavidbarton\.github\.io\//gi)) {
       response = await fetch('https://twin-peaks-api.herokuapp.com/api/quotes/recommend')
-      await response.json().then(function(data) {
+      await response.json().then(function (data) {
         obj = data[0]
       })
     } else {
       response =
-        '[{"quoteTextOnly":"[DEV MODE] The owls are not what they seem.","persons":["The Giant","Person Name"]}]'
+        '[{"quoteTextOnly":"There\'s a sort of evil out there. Something very, very strange in these old woods. Call it what you want. A darkness, a presence. It takes many forms but... its been out there for as long as anyone can remember and we\'ve always been here to fight it. [DEV MODE]","persons":["Sheriff Truman"]}]'
       obj = JSON.parse(response)[0]
+      console.log('DEV MODE: on')
     }
 
     let quoteTextOnly = obj.quoteTextOnly.replace(
       /bitch|fuck|shit|whore|cunt|titties/gi, // some censorship on frequent naughty words in the quotes
       '<span class="bg-secondary">[obscenity]</span>'
     )
-    quoteTextOnly.length >= 380
-      ? (quoteTextOnly = quoteTextOnly.substring(0, 380) + '...')
-      : (quoteTextOnly = quoteTextOnly)
+    quoteTextOnly.length >= 380 ?
+      (quoteTextOnly = quoteTextOnly.substring(0, 380) + '...') :
+      (quoteTextOnly = quoteTextOnly)
     const persons = obj.persons.map(el => el)
     document.querySelector('#twinpeaks-quote > p').innerHTML = quoteTextOnly
     document.querySelector('#twinpeaks-quote > footer > cite').innerHTML = persons
