8f20146cd294d8dce3ffe3ce1d262a69258e5596
David Barton <barton.david.sv@gmail.com>
2020-10-10 14:23:08 +0200

    refactor project and add view counter

--- header/index.js
+++ header/index.js
@@ -12,8 +12,8 @@ function getLatestArticle() {
   fetch('/header/latestArticle.json')
     .then(response => response.json())
     .then(json => {
-      document.querySelector('#cookie-bar-link').href = `/blog/${json.slug}`
-      document.querySelector('#cookie-bar-link').innerText =
+      document.querySelector('#top-bar-link').href = `/blog/${json.slug}`
+      document.querySelector('#top-bar-link').innerText =
         json.title.replace(/^(.{40}[^\s]*).*/, '$1') + (json.title.length > 40 ? '...' : '')
     })
 }
