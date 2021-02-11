5653ba17bbc5ab9798f6def87b06d938315e83cb
David Barton <barton.david.sv@gmail.com>
2021-02-11 23:48:30 +0100

    replace cdn img links and finetune topbar

--- header/index.js
+++ header/index.js
@@ -1,5 +1,5 @@
 // load latest article on top
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener('DOMContentLoaded', () => {
   fetch('/header/latestArticle.json')
     .then(response => response.json())
     .then(json => {
@@ -7,4 +7,12 @@ document.addEventListener('DOMContentLoaded', async () => {
       document.querySelector('#top-bar-link').innerText =
         json.title.replace(/^(.{40}[^\s]*).*/, '$1') + (json.title.length > 40 ? '...' : '')
     })
+})
+
+// transition of top bar
+document.addEventListener('DOMContentLoaded', () => {
+  const topBar = document.querySelector('#top-bar-text')
+  window.setTimeout(() => {
+    topBar.style.opacity = '1'
+  }, 500)
 })
