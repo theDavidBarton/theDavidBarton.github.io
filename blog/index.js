40eebcc3342541dfedf42368574f407f329e0764
David Barton <43063460+theDavidBarton@users.noreply.github.com>
2022-07-07 23:30:37 +0200

    feat(blog): pretty code blocks (#175)

--- blog/index.js
+++ blog/index.js
@@ -12,5 +12,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const prismScript = document.createElement('script')
     prismScript.src = '/assets/prism.js'
     document.body.appendChild(prismScript)
+    // these old articles used very short snippets that looks bad with the wider code block
+    if (window.location.href.match(/anonymous-IIFE-fn|os-dependent-emoji-display|devtools-dollar-shorthand-jquery/)) {
+      const pres = document.querySelectorAll('pre')
+      pres.forEach(el => el.style.setProperty('margin', '1em 0', 'important'))
+    }
   }
 })
